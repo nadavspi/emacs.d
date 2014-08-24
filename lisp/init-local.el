557cd22fc34532c0b18f168190045772fc9d812c
Nadav Spiegelman <me@nadav.name>
2014-08-24 14:06:59 -0400

    Add evil, line numbers, remove toolbar

---
+++ lisp/init-local.el
@@ -0,0 +1,15 @@
+(require-package 'evil)
+
+(setq evil-search-module 'evil-search
+      evil-want-C-u-scroll t
+      evil-want-C-w-in-emacs-state t)
+
+(require 'evil)
+(evil-mode t)
+
+; add line numbers
+(global-linum-mode t)
+
+(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(unless (display-graphic-p) (menu-bar-mode -1))
