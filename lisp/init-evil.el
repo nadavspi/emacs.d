ddccbf206f3102ac16ef8a3bf1fa999848975830
Nadav Spiegelman <me@nadav.name>
2014-08-25 20:55:01 -0400

    Add evil-leader, map <leader>gs to magit-status

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -1,11 +1,18 @@
 (require-package 'evil)
+(require-package 'evil-leader)
 
 (setq evil-search-module 'evil-search
       evil-want-C-u-scroll t
       evil-want-C-w-in-emacs-state t)
 
 (require 'evil)
+(require 'evil-leader)
+(global-evil-leader-mode)
 (evil-mode t)
+
+(evil-leader/set-leader ",")
+(evil-leader/set-key
+ "gs" 'magit-status)
 
 (setq evil-insert-state-cursor '("grey" line))
 (setq evil-emacs-state-cursor '("grey" box))
